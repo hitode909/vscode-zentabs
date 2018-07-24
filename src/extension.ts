34407bcf9c67310d2bd64d4dac3c7835d043c37f
hitode909 <hitode909@gmail.com>
2018-07-24 09:38:39 +0900

    initial broken version

--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,106 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+class HistoryRepository {
+    inGC: boolean = false;
+    readonly max: number;
+    constructor(max: number) {
+        this.max = max;
+    }
+
+    items: Array<HistoryItem> = [];
+
+    startGC():void {
+        this.inGC = true;
+    }
+    stopGC():void {
+        this.inGC = false;
+    }
+
+    willClose(item: HistoryItem): boolean {
+        const itemInRepository = this.items.find(i => i.uri === item.uri);
+        if (itemInRepository) {
+            return itemInRepository.willClose;
+        }
+        return false;
+    }
+
+    push(item: HistoryItem): void {
+        this.items.unshift(item);
+
+        const knownUris: { [key: string]: boolean } = {};
+        const newHistory: Array<HistoryItem> = [];
+        this.items.forEach(item => {
+            if (item.editor.document.isClosed) {
+                return;
+            }
+            if (knownUris[item.uri]) {
+                return;
+            }
+            knownUris[item.uri] = true;
+            newHistory.push(item);
+        });
+        this.items = newHistory;
+    }
+
+    getItemToTrim(): HistoryItem | undefined {
+        if (this.items.length > this.max) {
+            return this.items.pop();
+        }
+    }
+}
+class HistoryItem {
+    readonly editor: vscode.TextEditor;
+    willClose: boolean = false;
+
+    get uri(): string {
+        return this.editor.document.uri.toString();
+    }
+
+    constructor(editor: vscode.TextEditor) {
+        this.editor = editor;
+    }
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "zentabs" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    const repository = new HistoryRepository(3);
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    vscode.window.onDidChangeActiveTextEditor(async editor => {
+        try {
+            if (!editor) { return; }
+            if (repository.inGC) {
+                console.log('in GC');
+                return;
+            }
+            const item = new HistoryItem(editor);
+            if (repository.willClose(item)) {
+                return;
+            }
 
-    context.subscriptions.push(disposable);
+            repository.push(item);
+            console.log(`append ${item.uri}`);
+            console.log(repository.items.map(i => `- ${i.uri}`).join("\n"));
+            const itemToTrim = repository.getItemToTrim();
+            if (itemToTrim) {
+                repository.startGC();
+                itemToTrim.willClose = true;
+                console.log(`close ${itemToTrim.uri}`);
+                await vscode.window.showTextDocument(itemToTrim.editor.document);
+                await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+                if (!editor.document.isClosed) {
+                    await vscode.window.showTextDocument(editor.document);
+                }
+                repository.stopGC();
+            }
+        } catch (error) {
+            repository.stopGC();
+        }
+    }, null, context.subscriptions);
 }
 
 // this method is called when your extension is deactivated
